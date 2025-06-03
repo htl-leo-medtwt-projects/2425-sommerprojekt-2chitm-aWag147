9b0f0e2f87a8a6590d424ad11f3fda2125e14cdb
Adrian Wagner <a.wagner@students.htl-leonding.ac.at>
2025-06-03 09:51:07 +0200

    FormatNumber verbessert (Vergrößert)

--- project/files/game.js
+++ project/files/game.js
@@ -244,29 +244,49 @@ function resetGame(){
 }
 
 function formatNumberFixed(num) {
-    if (num >= 1000000000) {
+    if (num >= 1000000000000) {
+        return (num / 1000000000000).toFixed(2) + ' T';
+    } else if (num >= 1000000000) {
         return (num / 1000000000).toFixed(2) + ' B'; 
     } else if (num >= 1000000) {
         return (num / 1000000).toFixed(2) + ' M';  
     } else if (num >= 1000) {
         return (num / 1000).toFixed(2) + ' K'; 
+    } else if (num <= -1000000000000) {
+        return (num / 1000000000000).toFixed(2) + ' T';
+    } else if (num <= -1000000000) {
+        return (num / 1000000000).toFixed(2) + ' B';
+    } else if (num <= -1000000) {
+        return (num / 1000000).toFixed(2) + ' M';
+    } else if (num <= -1000) {
+        return (num / 1000).toFixed(2) + ' K';
     } else {
         return num;
     }
 }
 
-
 function formatNumber(num) {
-    if (num >= 1000000000) {
+    if (num >= 1000000000000) {
+        return (num / 1000000000000).toFixed(0) + 'T';
+    } else if (num >= 1000000000) {
         return (num / 1000000000).toFixed(0) + 'B';
     } else if (num >= 1000000) {
         return (num / 1000000).toFixed(0) + 'M';  
     } else if (num >= 1000) {
         return (num / 1000).toFixed(0) + 'K';  
+    } else if (num <= -1000000000000) {
+        return (num / 1000000000000).toFixed(0) + 'T';
+    } else if (num <= -1000000000) {
+        return (num / 1000000000).toFixed(0) + 'B';
+    } else if (num <= -1000000) {
+        return (num / 1000000).toFixed(0) + 'M';
+    } else if (num <= -1000) {
+        return (num / 1000).toFixed(0) + 'K';
     } else {
         return num;
     }
 }
+
 
 
 //Chat
